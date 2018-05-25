e8640800c77befda7185e42b231c445c20e8bb2a
briefausde <sirkiza@gmail.com>
2018-05-25 17:39:32 +0300

    deploy fix

--- appliances/settings.py
+++ appliances/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
